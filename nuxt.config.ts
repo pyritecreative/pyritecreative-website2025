02f59cea8c55b79657a9a1e551f33f1b96f02528
Jamie Sanders <jamiesanders@gmail.com>
2025-01-30 17:18:27 +0000

    updated studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "nuxt-time", "@nuxthq/studio"],
+  modules: [
+    "@nuxt/content",
+    "@nuxtjs/tailwindcss",
+    "nuxt-time",
+    "@nuxthq/studio",
+  ],
   devtools: { enabled: true },
-  future: {
-    compatibilityVersion: 4,
-  },
-  compatibilityDate: "2024-04-03",
-  runtimeConfig: {
-    // The private keys which are only available server-side
-    apiSecret: "123",
-    // Keys within public are also exposed client-side
-    public: {
-      apiBase: "/api",
-    },
-  },
-  content: {
-    preview: {
-      api: "https://api.nuxt.studio",
-    },
-  },
+  // future: {
+  //   compatibilityVersion: 4,
+  // },
+  // compatibilityDate: "2024-04-03",
+  // runtimeConfig: {
+  //   // The private keys which are only available server-side
+  //   apiSecret: "123",
+  //   // Keys within public are also exposed client-side
+  //   public: {
+  //     apiBase: "/api",
+  //   },
+  // },
+  // content: {
+  //   preview: {
+  //     api: "https://api.nuxt.studio",
+  //   },
+  // },
   // https://nuxt.com/docs/getting-started/styling
   // app: {
   //   head: {
