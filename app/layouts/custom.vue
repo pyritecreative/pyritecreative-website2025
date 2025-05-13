e04acd258d064d7f135120ace08a8ef6f0deb0e0
Jamie Sanders <jamiesanders@gmail.com>
2025-05-13 16:57:23 +0100

    added layout

---
+++ app/layouts/custom.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
+
+<style></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <p>DEFAULT layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
+
+<style></style>
